718480d51be35b45ccff3994c7cd3c82dce1c5d1
Brian Eyring <brian20268@gmail.com>
2012-01-10 22:50:46 -0500

    Chart Controller

---
+++ ChartController.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class ChartController here.
+ * 
+ * @Brian Eyring 
+ * @Assignment 6.5
+ */
+import java.util.Scanner;
+public class ChartController
+{
+   public static void main(String[] args)
+   {
+        int value = 0;
+        Scanner scan = new Scanner(System.in);
+        Histogram hist = new Histogram(new int[][]{{0, 10}, {11, 20}, {21, 30}, {31, 40}, {41, 50}, {51, 60}, {61, 70}, {71, 80}, {81, 90}, {91, 100}});
+        while (value != -1)
+        {
+            System.out.println("Enter a value you wish to enter into the chart(-1 to quit): ");
+            value = scan.nextInt();
+            if (value != -1)
+            {
+                hist.addValue(value);
+                System.out.println(hist);
+            }
+        }
+    }
+}
